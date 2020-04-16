d7e4abbfe35e513b85d2f06b27957dc6c7faa53d
RaihanArdianata <bxgxxxx@gmail.com>
2020-04-16 20:35:50 +0700

    done add new action new game and instance player

--- tictactoe/src/store/index.js
+++ tictactoe/src/store/index.js
@@ -1,14 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Player from './player.js'
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:3000')
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    P1: 'X',
+    P2: 'O',
+    player: null,
+    game: null
   },
   mutations: {
+    SET_PLAYER: function (state, payload) {
+      state.player = payload
+    }
   },
   actions: {
+    // start game
+    newGame: function (context, payload) {
+      const name = payload.name
+      return new Promise((resolve, reject) => {
+        if (!name) {
+          const msg = 'Please enter your name.'
+          return reject(msg)
+        } else {
+          const msg = name + 'success created'
+          socket.emit('createGame', { name })
+          context.commit('SET_PLAYER', new Player(name, this.p1))
+          return resolve(msg)
+        }
+      })
+    }
   },
   modules: {
   }
